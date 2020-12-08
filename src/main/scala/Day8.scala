5d98fa439dac557f7cd3ecb7660d5aead443ba7a
Zoë Mackay <zoe.mackay@ovoenergy.com>
2020-12-08 12:45:57 +0000

    Tidy up

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -22,8 +22,7 @@ sealed trait Result {
 }
 case class Terminates(accumulatorValue : Int) extends Result
 case class InfiniteLoop(accumulatorValue : Int) extends Result
-
-
+  
 object Operation {
 
   def fromString(input : String) : Option[Operation] = {
@@ -41,52 +40,55 @@ object Operation {
 }
 
 object Day8 extends App {
-  val fileName = "/home/zoe/Work/Repositories/AdventOfCode/src/main/resources/Day8TestData"
-  val lines = scala.io.Source.fromFile(fileName).getLines().toList
-  val program = lines.flatMap(Operation.fromString)
 
-  def replaceX(x : Int, operation: Operation, program : List[Operation]) : List[Operation] = {
-    program.take(x) ++ (operation :: program.drop(x + 1))
+  def initialState = ComputerState(0,0)
+
+  def replaceOperation(index : Int, operation: Operation, program : List[Operation]) : List[Operation] = {
+    program.take(index) ++ (operation :: program.drop(index + 1))
   }
 
-  def switchAroo(x : Int, program : List[Operation]) : Option[List[Operation]] = {
-    program(x) match {
-      case Jmp(value) => Some(replaceX(x, NoOp(value), program))
-      case NoOp(value) => Some(replaceX(x, Jmp(value), program))
+  def switchAroo(index : Int, program : List[Operation]) : Option[List[Operation]] = {
+    program(index) match {
+      case Jmp(value) => Some(replaceOperation(index, NoOp(value), program))
+      case NoOp(value) => Some(replaceOperation(index, Jmp(value), program))
       case _ => None
     }
   }
 
   def executeUntilEnds(computerState: ComputerState, program : List[Operation], executedInstructions : Set[Int]) : Result =
-    if (executedInstructions.contains(computerState.programCounter)) InfiniteLoop(computerState.accumulator)
+    if
+      (executedInstructions.contains(computerState.programCounter)) InfiniteLoop(computerState.accumulator)
     else
-      if (computerState.programCounter >= program.size) Terminates(computerState.accumulator)
-      else executeUntilEnds(program(computerState.programCounter).perform(computerState), program, executedInstructions + computerState.programCounter)
+      if
+        (computerState.programCounter >= program.size) Terminates(computerState.accumulator)
+      else
+        executeUntilEnds(program(computerState.programCounter).perform(computerState), program, executedInstructions + computerState.programCounter)
 
-  def checkOptions(options : List[List[Operation]]) : Option[Result] = {
-    options match {
+  def checkCandidates(candidates : List[List[Operation]]) : Option[Result] = {
+    candidates match {
       case Nil => None
-      case program :: remainingOptions => {
-        val executedResult = executeUntilEnds(ComputerState(0,0), program, Set.empty)
+      case program :: remainingCandidates => {
+        val executedResult = executeUntilEnds(initialState, program, Set.empty)
         executedResult match {
-          case Terminates(_) => {
-            println(program)
-            Some(executedResult)
-          }
-          case _ => checkOptions(remainingOptions)
+          case Terminates(_) => Some(executedResult)
+          case _ => checkCandidates(remainingCandidates)
         }
       }
     }
   }
 
   def findAnswer1(program : List[Operation]) : Int = {
-    executeUntilEnds(ComputerState(0,0), program, Set.empty).accumulatorValue
+    executeUntilEnds(initialState, program, Set.empty).accumulatorValue
   }
 
   def findAnswer2(program : List[Operation]) : Int = {
-    val options = program.indices.flatMap(x => switchAroo(x, program)).toList
-    checkOptions(options).map(_.accumulatorValue).getOrElse(-1)
+    val candidates = program.indices.flatMap(x => switchAroo(x, program)).toList
+    checkCandidates(candidates).map(_.accumulatorValue).getOrElse(-1)
   }
+
+  val fileName = "/home/zoe/Work/Repositories/AdventOfCode/src/main/resources/Day8TestData"
+  val lines = scala.io.Source.fromFile(fileName).getLines().toList
+  val program = lines.flatMap(Operation.fromString)
 
   println(findAnswer1(program))
   println(findAnswer2(program))
