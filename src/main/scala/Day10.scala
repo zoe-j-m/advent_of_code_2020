535ffbbe5ff9664004565168a20188253a0ae8f3
Zoë Mackay <zoe.mackay@ovoenergy.com>
2020-12-10 11:11:44 +0000

    Day 10

---
+++ src/main/scala/Day10.scala
@@ -0,0 +1,42 @@
+object Day10 extends App {
+  def getGaps(chargers : List[Long]):List[Long] = chargers.sorted.sliding(2).map(pair => pair.max - pair.min).toList
+
+  def getGapMap(chargers : List[Long]) : Map[Long, Long] = getGaps(chargers).groupBy(a => a).map(a => (a._1, a._2.size))
+
+  def getWaysICouldGoForward(from: Long, remaining : List[Long], soFar : Long) : Long = {
+    remaining match {
+      case Nil => 1
+      case _ => {
+        val options = remaining.takeWhile(_ <= from + 3L)
+          options.map(
+            newTarget => getWaysICouldGoForward(newTarget, remaining.dropWhile(_ <= newTarget), soFar)).sum
+      }
+    }
+  }
+
+  def breakdownIntoThreeGapped(current : List[Long], remaining : List[Long]): List[List[Long]] =
+  {
+    remaining match {
+      case Nil => List(current.reverse)
+      case x :: xs if x - current.head == 3L => current.reverse :: breakdownIntoThreeGapped(List(x), xs)
+      case x :: xs => breakdownIntoThreeGapped(x :: current, xs)
+    }
+  }
+
+  def findAnswer1(input: List[Long]): Long = {
+    val mappedGaps = getGapMap(0L :: input)
+    mappedGaps(1L) * (mappedGaps(3L) + 1L)
+  }
+
+  def findAnswer2(input: List[Long]): Long = {
+    val threeGapped = breakdownIntoThreeGapped(List(0L), input.sorted)
+    val subResults = threeGapped.map(subList => getWaysICouldGoForward(subList.head, subList.tail, 1))
+    subResults.foldLeft(1L)(_ * _)
+  }
+
+  val fileName = "/home/zoe/Work/Repositories/AdventOfCode/src/main/resources/Day10TestData"
+  val lines = scala.io.Source.fromFile(fileName).getLines().toList.map(_.toLong)
+
+  println(findAnswer1(lines))
+  println(findAnswer2(lines))
+}
---
+++ src/test/scala/Day10Spec.scala
@@ -0,0 +1,29 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class Day10Spec extends AnyFlatSpec {
+
+  val example2 = "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3".split('\n').toList.map(_.toLong)
+
+  val example1 = List(1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19).map(_.toLong)
+
+  "findAnswer1" should "give correct result" in {
+     Day10.findAnswer1(example2) shouldBe 220
+  }
+
+  "findAnswer2" should "give correct result" in {
+    Day10.findAnswer2(example1) shouldBe 8
+  }
+
+  "breakdown into three gapped" should "give back a list divided into three-gapped segments" in {
+    Day10.breakdownIntoThreeGapped(List(0L), example1 ++ List(22L)) shouldBe List(List(0L, 1L), List(4L,5L,6L, 7L), List(10L, 11L,12L), List(15L, 16L))
+  }
+
+
+  "findAnswer2" should "give correct result2" in {
+    Day10.findAnswer2(example2) shouldBe 19208L
+  }
+
+
+
+}
